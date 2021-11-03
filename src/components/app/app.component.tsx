8cc3f085d63dbf5706deec282302da6f1a646a7e
Dany Pignoux <dany.pignoux@outlook.fr>
2021-11-03 12:29:33 +0100

    Initial commit

---
+++ src/components/app/app.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react-dom';
+
+/**
+ * Represent the application root component
+ */
+export function AppComponent(): any {
+  return <div className="container">
+  </div>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react-dom';
+import {AppComponent} from './components/app/app.component';
+
+React.render(
+  <AppComponent/>,
+  document.querySelector('#app')
+);
